ed02538c89804221c5f166eb658ea44bd6602641
Paul Brooker <pdbrooker@hotmail.com>
2016-04-21 14:58:28 -0300

    Add project files.

---
+++ MyTinyCollege/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTinyCollege.Models
+{
+    public class Person
+    {
+
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string Email { get; set; }
+
+        //Full name is a calculated property that returns a value created 
+        //by concatenating two other properties therefore it only has a get accessor,
+        //and because of this no FullName column will be generated in the database
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+    }
+}
---
+++ MyTinyCollege/Models/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTinyCollege.Models
+{
+    public class Student:Person
+    {
+        public DateTime EnrollmentDate { get; set; }
+
+        //1 student with many enrollments
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
