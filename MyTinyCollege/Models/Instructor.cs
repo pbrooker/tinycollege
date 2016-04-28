a8eb5e3611bdff451495d246ed407c1d2a8b0f38
Paul Brooker <pdbrooker@hotmail.com>
2016-04-28 09:13:23 -0300

    added new scaffold. Fixed typo in Instructor model

--- MyTinyCollege/Models/Instructor.cs
+++ MyTinyCollege/Models/Instructor.cs
@@ -9,7 +9,7 @@ namespace MyTinyCollege.Models
     public class Instructor:Person
     {
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0;yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
 
